d305a341ee8d3818065cd34b445bfae8f5fe8ca3
Adam Nuridov <adamnuri92@gmail.com>
2021-11-20 10:14:45 +0100

    clockband app

---
+++ apps/clockband/app.js
@@ -0,0 +1,41 @@
+Bangle.on('accel', function(acc) {
+  var magnitude = acc.mag.toFixed(1);
+  if(magnitude != 1.0) {
+    console.log(acc.mag.toFixed(1));
+  }
+});
+
+// Two variables to update
+var heartrateDrum = false;
+var accel = false;
+// First menu
+var mainmenu = {
+  "" : {
+    "title" : "-- Clock Band --"
+  },
+  "Hjerterytme drums" : {
+    value : heartrateDrum,
+    format : v => v?"På":"Av",
+    onchange : v => { 
+      heartrateDrum=v; 
+      Bangle.setHRMPower(v ? 1 : 0);
+Bangle.on('HRM',function(hrm) {
+  /*hrm is an object containing:
+    { "bpm": number,             // Beats per minute
+      "confidence": number,      // 0-100 percentage confidence in the heart rate
+      "raw": Uint8Array,         // raw samples from heart rate monitor
+   */
+  console.log(hrm.bpm, hrm.confidence);
+});
+    }
+  },
+  "Accel" : {
+    value : accel,
+    format : v => v?"På":"Av",
+    onchange : v => { accel=v; }
+  },
+  "Exit" : function() { E.showMenu(); },
+};
+
+
+E.showMenu(mainmenu);
