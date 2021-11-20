65f774a9382398ee7ce58ea1b2601ec54ec6b43d
Philipp Zirpins <philipp.zirpins@bouvet.no>
2021-11-20 16:12:52 +0100

    accelerometer test app v0.8

--- apps/clock_band/app.js
+++ apps/clock_band/app.js
@@ -1,41 +1,16 @@
-Bangle.on('accel', function(acc) {
-  var magnitude = acc.mag.toFixed(1);
-  if(magnitude != 1.0) {
-    console.log(acc.mag.toFixed(1));
-  }
-});
 
-// Two variables to update
-var heartrateDrum = false;
-var accel = false;
-// First menu
-var mainmenu = {
-  "" : {
-    "title" : "-- clock_band --"
-  },
-  "Hjerterytme drums" : {
-    value : heartrateDrum,
-    format : v => v?"På":"Av",
-    onchange : v => { 
-      heartrateDrum=v; 
-      Bangle.setHRMPower(v ? 1 : 0);
-Bangle.on('HRM',function(hrm) {
-  /*hrm is an object containing:
-    { "bpm": number,             // Beats per minute
-      "confidence": number,      // 0-100 percentage confidence in the heart rate
-      "raw": Uint8Array,         // raw samples from heart rate monitor
-   */
-  console.log(hrm.bpm, hrm.confidence);
-});
-    }
-  },
-  "Accel" : {
-    value : accel,
-    format : v => v?"På":"Av",
-    onchange : v => { accel=v; }
-  },
-  "Exit" : function() { E.showMenu(); },
-};
+var accel = Bangle.getAccel();
 
+function draw(){
+  // read accelerometer
+  accel = Bangle.getAccel();
+  consoleDebug(accel.x);
+  consoleDebug(accel.y);
+  consoleDebug(accel.z);
 
-E.showMenu(mainmenu);
+  // prepares display for new image to be drawn?
+  g.flip();
+  g.setFont("6x8",3);
+  g.drawString(accel,13,11);
+}
+setInterval(draw, 100);
